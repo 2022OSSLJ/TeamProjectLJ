59d3511d7ca7be49eb90caf903830ab574b71628
Woo Hyung <21600699@handong.ac.kr>
2022-05-08 15:31:45 +0900

    create main, update car.h

--- car.h
+++ car.h
@@ -14,9 +14,10 @@ int selectMenu();
 void saveData(Car *c, int count);
 int loadData(Car *c);
 
-
 void searchByType(Car *c);
 void searchByModel(Car *c);
 void searchByLocation(Car *c);
 void searchByPrice(Car *c);
 void searchByMileage(Car *c);
+
+int selectMenu();//메뉴 표출
