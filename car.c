e5775968f9a082b367cd0a8f92438ca938450405
Woo Hyung <21600699@handong.ac.kr>
2022-05-08 15:37:46 +0900

    create car.c

--- car.c
+++ car.c
@@ -1,2 +1,21 @@
-#include car.h
+#include <stdio.h>
+#include <string.h>
+#include "car.h"
+int createCar(Car *c){
+	
+}
+void readCar(Car *c);
+int updateCar(Car *c);
+int deleteCar(Car *c);
+int selectMenu();
+void saveData(Car *c, int count);
+int loadData(Car *c);
+
+void searchByType(Car *c);
+void searchByModel(Car *c);
+void searchByLocation(Car *c);
+void searchByPrice(Car *c);
+void searchByMileage(Car *c);
+
+int selectMenu();//메뉴 표출
 
